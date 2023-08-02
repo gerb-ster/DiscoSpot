41ce980eb7b31fe4e1e520df865429bc5b46ce6d
Gerbrand Sterrenburg <gerb@gerbster.nl>
2023-08-02 13:24:44 +0200

    no message

--- web-app/resources/js/Pages/LandingPage.vue
+++ web-app/resources/js/Pages/LandingPage.vue
@@ -1,39 +1,43 @@
 <template>
     <Head :title="$t('landingPage.title')" />
-    <div class="d-flex align-center flex-column">
-        <v-card
-            width="400"
-            class="mt-10"
-        >
-            <v-img
-                class="logoBackground"
-                height="200px"
-                src="/images/logo_placeholder.png"
-                contain
-            />
-            <v-card-title
-                class="text-h5 mb-4 mt-2"
-            >
-                {{ $t('landingPage.title') }}
-            </v-card-title>
-            <v-card-text
-                class="text-body-1"
-            >
-                {{ $t('landingPage.text') }}
-            </v-card-text>
-            <v-card-actions>
-                <v-btn
-                    prepend-icon="mdi-rocket-launch"
-                    size="large"
-                    variant="tonal"
-                    class="flex-grow-1 mt-5"
-                    :href="$route('auth.discogs.connect')"
+    <v-app id="layout">
+        <v-main class="backgroundGradient">
+            <div class="d-flex align-center flex-column">
+                <v-card
+                    width="400"
+                    class="mt-10"
                 >
-                    {{ $t('landingPage.startBtn') }}
-                </v-btn>
-            </v-card-actions>
-        </v-card>
-    </div>
+                    <v-img
+                        class="logoBackground"
+                        height="200px"
+                        src="/images/logo_placeholder.png"
+                        contain
+                    />
+                    <v-card-title
+                        class="text-h5 mb-4 mt-2"
+                    >
+                        {{ $t('landingPage.title') }}
+                    </v-card-title>
+                    <v-card-text
+                        class="text-body-1"
+                    >
+                        {{ $t('landingPage.text') }}
+                    </v-card-text>
+                    <v-card-actions>
+                        <v-btn
+                            prepend-icon="mdi-rocket-launch"
+                            size="large"
+                            variant="tonal"
+                            class="flex-grow-1 mt-5"
+                            :href="$route('auth.discogs.connect')"
+                        >
+                            {{ $t('landingPage.startBtn') }}
+                        </v-btn>
+                    </v-card-actions>
+                </v-card>
+            </div>
+        </v-main>
+    </v-app>
 </template>
 
 <script setup>
